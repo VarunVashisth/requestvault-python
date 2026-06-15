bf27b60794fe9eaf9dea6fcbeb7c893101c7659f
VarunVashisth <varun123shivam@gmail.com>
2026-06-15 13:23:51 +0530

    flush

--- sdk_test.py
+++ sdk_test.py
@@ -1,19 +1,60 @@
+
+
 from requestvault import RequestVault
-import requests , time
+import requests
+import time
 
 RequestVault.init(
-    api_key="rv_o56NwCIz9s-JZHX1jl3wVbIIp_81QEaDV2V_h5MYPGs",
-    capture_headers=False,
-    debug=True,
-    )
+    api_key="rv_test",
+    debug=True
+)
 
+print("Creating test traffic...")
 
-
+# Success GET
 try:
-        requests.get("https://httpbin.org/get")
+    requests.get("https://httpbin.org/get")
 except:
-    pass 
+    pass
 
-time.sleep(5)
+# Success POST
+try:
+    requests.post(
+        "https://httpbin.org/post",
+        json={"name": "varun"}
+    )
+except:
+    pass
+
+# 404
+try:
+    requests.get(
+        "https://httpbin.org/status/404"
+    )
+except:
+    pass
+
+# 500
+try:
+    requests.get(
+        "https://httpbin.org/status/500"
+    )
+except:
+    pass
+
+# Timeout / failed request
+try:
+    requests.get(
+        "https://this-domain-does-not-exist-123456.com",
+        timeout=2
+    )
+except:
+    pass
 
 
+
+print("Waiting for SDK worker flush...")
+
+time.sleep(10)
+
+print("Done")
